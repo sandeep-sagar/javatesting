068674a89f3e042506b28020744d68d67949c2bc
Sandeep Sagar <sandeep.s.sagar@gmail.com>
2019-04-05 01:05:07 +0530

    adding loop function

---
+++ loop.java
@@ -0,0 +1,18 @@
+public class loop 
+	{
+	public static void main(String[] args){
+		public static void main(String[] args) {
+		for(int i= 0 ; i<=4; i++) {
+			for(int j= 0 ; j<=i; j++) {
+				if(i==0 || i==5-i || j==4)
+				System.out.print("hi");
+			}
+			else
+			{
+			System.out.print("");	
+		}
+		}
+			System.out.println("");
+		
+	}
+	}
