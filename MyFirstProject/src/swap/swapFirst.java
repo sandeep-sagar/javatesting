400b48812c8f75e1d280e4dbd22d8bbad23a8b1c
Sandeep Sagar <sandeep.s.sagar@gmail.com>
2019-04-05 02:22:51 +0530

    Modifiedwth Arithmatic operator

--- MyFirstProject/src/swap/swapFirst.java
+++ MyFirstProject/src/swap/swapFirst.java
@@ -1,21 +1,30 @@
 package swap;
 
 public class swapFirst {
-	public static void swapWithTemp() {
+	public static void swapWitArithmatic(int a, int b) {
+		System.out.println("\n*****Arithmatic opertaor*****");
+		System.out.println("originalvalue of a:"+a +"\nOriginal value of b:" +b);
+		a = a + b;
+		b = a- b;
+		a = a - b;
+		System.out.println("Changed value of a:"+a + "\nChanged value of b:" +b);
 		
-		int a = 10;
-		int b = 20;
+	}
+	public static void swapWithTemp(int a, int b) {
 		int temp;
-		//temp = a;
-		System.out.println("originalvalue of a:"+a +"\n Original value of b:" +b);
+		System.out.println("\n================ Swap with temp var ===============");
+		System.out.println("originalvalue of a:"+a +"\nOriginal value of b:" +b);
 		temp = b;
 		b = a;
 		a = temp;
-		System.out.println("Changed value of a:"+a + "\n Changed value of b:" +b);
+		System.out.println("Changed value of a:"+a + "\nChanged value of b:" +b);
 	}
 	
 	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
 		swapFirst swap = new swapFirst(); 
-		swap.swapWithTemp();
+		swap.swapWithTemp(a,b);
+		swap.swapWitArithmatic(a,b);
 	}
 }
