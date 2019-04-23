9b7f5d56f84bcb00417895aa5412f9a56643a22f
Sandeep Sagar <sandeep.s.sagar@gmail.com>
2019-04-24 01:47:38 +0530

    changesin revise

--- MyFirstProject/src/revise/triangleProg.java
+++ MyFirstProject/src/revise/triangleProg.java
@@ -9,9 +9,24 @@ public class triangleProg {
 			System.out.println();
 		}
 	}
+	public static void tri() {
+		for(int i = 0 ; i<= 3 ; i++) {
+			for(int j = 0 ; j<= 3 ; j++) {
+				if( i== 3 || j == 0 || j <= i) {
+					System.out.print("*");
+				} else {
+					System.out.print("");
+				}
+			}
+			System.out.println();
+		}
+	}
 	
 	public static void main(String[] args) {
 		triangleProg tg= new triangleProg();
 		triangleProg.triangle();
+		System.out.println();
+		triangleProg tri = new triangleProg();
+		triangleProg.tri();
 	}
 }
